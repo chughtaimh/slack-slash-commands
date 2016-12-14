a5d522d70d37eb00795cc299be7baeecef767b4b
Moe <chughtai.mh@gmail.com>
2016-12-14 16:48:39 -0500

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-import webapp2
+import pickle
 import random
+import webapp2
+
+from weather import weather
 
 
 class Home(webapp2.RequestHandler):
@@ -18,14 +21,41 @@ class WeatherApp(webapp2.RequestHandler):
 		"""Receives a POST request"""
 
 		zip_code = self.request.get('text')
+		user_id = self.request.get('user_id')
 
-		get_weather(zip_code, handler=self)
-		
+		if not zip_code:
+			zip_code = self._get_saved_zip_code(user_id)
 
-	def get_weather(self, zip_code, handler):
+		self.get_weather(str(zip_code))
+		self._save_user_zip_code(user_id, zip_code)
+
+	def get_weather(self, zip_code):
 		"""Gets weather for :zip_code: and writes to :handler:"""
 
-		handler.response.write('Getting Weather!')
+		if zip_code:
+			_weather = weather(str(zip_code))
+			self.response.write(_weather)
+		else:
+			self.response.write('No zip code entered...')
+
+	def _get_saved_zip_code(self, user_id):
+		"""Attempts to return saved zip code for :user_id:"""
+
+		with open('saved_user_zips.p', 'rb') as f:
+			user_zips = pickle.load(f)
+			return user_zips.get(user_id)
+
+	def _save_user_zip_code(self, user_id, zip_code):
+		"""Saves dict[user_id] = zip_code to 'saved_user_zips.p'"""
+
+		with open('saved_user_zips.p', 'rb') as f:
+			user_zips = pickle.load(f)
+			user_zips[user_id] = zip_code 
+
+		with open('saved_user_zips.p', 'wb') as f:
+			pickle.dump(user_zips, f)
+
+
 
 
 app = webapp2.WSGIApplication([
